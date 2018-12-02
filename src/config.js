f7f39e1650d3961a4ce1d5183f1ed7b1a7fd9f4e
Daniel Nolan <DanNolan99@gmail.com>
2018-12-02 15:34:17 -0500

    API URL prefix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const PUBLIC_URL = process.env.REACT_APP_PUBLIC_URL || '/';
-const API_URL = process.env.API_URL || 'http://localhost:3002/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3002/';
 
 export {
   PUBLIC_URL,
